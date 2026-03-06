15ee060929a2e167a0dc845fa1da85609731d225
madduxj2 <jacob.maddux@peopleshores.com>
2026-03-06 14:42:14 -0500

    Completed PA 303.10.1 - Polymorphism and Inheritance

---
+++ src/PA_303_10_1/Monster.java
@@ -0,0 +1,13 @@
+package PA_303_10_1;
+
+public class Monster {
+    protected String name;
+
+    public Monster(String name) {
+        this.name = name;
+    }
+
+    public String attack() {
+        return "!^_&^$@+%$* I don't know how to attack!";
+    }
+}
---
+++ src/PA_303_10_1/WaterMonster.java
@@ -0,0 +1,13 @@
+package PA_303_10_1;
+
+public class WaterMonster extends Monster {
+
+    public WaterMonster(String name) {
+        super(name);
+    }
+
+    @Override
+    public String attack() {
+        return "Attack with water!";
+    }
+}
